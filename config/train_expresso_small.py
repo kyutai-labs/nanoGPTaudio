ba47daa352baf753da54006d6dd248326891dd9f
Vaclav Volhejn <vaclav@kyutai.org>
2025-08-12 18:35:25 +0200

    Train longer

--- config/train_expresso_small.py
+++ config/train_expresso_small.py
@@ -1,22 +1,19 @@
-# train a miniature character-level shakespeare model
-# good for debugging and playing on macbooks and such
-
 out_dir = 'out-expresso-small'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
-log_interval = 10 # don't print too too often
+log_interval = 50 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-wandb_log = False # override via command line if you like
-wandb_project = 'expresso-small'
-wandb_run_name = 'mini-gpt'
+wandb_log = True # override via command line if you like
+wandb_project = 'vaclav-nanogpt-audio'
+wandb_run_name = 'expresso-small'
 
-dataset = 'expresso_small'
-gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context size
+dataset = 'expresso'
+gradient_accumulation_steps = 8
+batch_size = 32
+block_size = 16384 # context size
 
 # baby GPT model :)
 n_layer = 6
@@ -25,8 +22,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 20000
+lr_decay_iters = 20000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
--- model.py
+++ model.py
@@ -288,7 +288,7 @@ class GPT(nn.Module):
         return optimizer
 
     def estimate_mfu(self, fwdbwd_per_iter, dt):
-        """ estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS """
+        """ estimate model flops utilization (MFU) in units of H100 bfloat16 peak FLOPS """
         # first estimate the number of flops we do per iteration.
         # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
         N = self.get_num_params()
@@ -297,9 +297,12 @@ class GPT(nn.Module):
         flops_per_token = 6*N + 12*L*H*Q*T
         flops_per_fwdbwd = flops_per_token * T
         flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
-        # express our flops throughput as ratio of A100 bfloat16 peak flops
+        # express our flops throughput as ratio of H100 bfloat16 peak flops
         flops_achieved = flops_per_iter * (1.0/dt) # per second
-        flops_promised = 312e12 # A100 GPU bfloat16 peak flops is 312 TFLOPS
+        # See https://www.ori.co/blog/choosing-between-nvidia-h100-vs-a100-performance-and-costs-considerations
+        # and https://developer.nvidia.com/blog/nvidia-hopper-architecture-in-depth/
+        # The numbers seem to be doubled in the first table because of the sparsity feature
+        flops_promised = 756e12 # H100 GPU bfloat16 peak flops
         mfu = flops_achieved / flops_promised
         return mfu
 
