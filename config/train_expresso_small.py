39a394698d6840308d9d4817eb050bf9ca7c85bb
Vaclav Volhejn <vaclav@kyutai.org>
2025-08-13 11:50:23 +0200

    Format everything

--- config/train_expresso_small.py
+++ config/train_expresso_small.py
@@ -1,19 +1,19 @@
-out_dir = 'out-expresso-small-block-size-2048'
-eval_interval = 250 # keep frequent because we'll overfit
+out_dir = "out-expresso-small-block-size-2048"
+eval_interval = 250  # keep frequent because we'll overfit
 eval_iters = 200
-log_interval = 50 # don't print too too often
+log_interval = 50  # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-wandb_log = True # override via command line if you like
-wandb_project = 'vaclav-nanogpt-audio'
-wandb_run_name = 'expresso-small-block-size-2048'
+wandb_log = True  # override via command line if you like
+wandb_project = "vaclav-nanogpt-audio"
+wandb_run_name = "expresso-small-block-size-2048"
 
-dataset = 'expresso'
+dataset = "expresso"
 gradient_accumulation_steps = 8
 batch_size = 32
-block_size = 2048 # context size
+block_size = 2048  # context size
 
 # baby GPT model :)
 n_layer = 6
@@ -21,13 +21,13 @@ n_head = 6
 n_embd = 384
 dropout = 0.2
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+learning_rate = 1e-3  # with baby networks can afford to go a bit higher
 max_iters = 20000
-lr_decay_iters = 20000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
-beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+lr_decay_iters = 20000  # make equal to max_iters usually
+min_lr = 1e-4  # learning_rate / 10 usually
+beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 100  # not super necessary potentially
 
 # on macbook also add
 # device = 'cpu'  # run on cpu only
--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 """
 Sample from a trained model
 """
+
 from datetime import datetime
 import os
 from pathlib import Path
@@ -16,62 +17,82 @@ import librosa
 from model import GPT, GPTConfig
 
 # -----------------------------------------------------------------------------
-init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-out_dir = 'out' # ignored if init_from is not 'resume'
-start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
-temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
-top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
+init_from = (
+    "resume"  # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+)
+out_dir = "out"  # ignored if init_from is not 'resume'
+start = "\n"  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+num_samples = 10  # number of samples to draw
+max_new_tokens = 500  # number of tokens generated in each sample
+temperature = (
+    0.8  # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+)
+top_k = (
+    200  # retain only the top_k most likely tokens, clamp others to have 0 probability
+)
 seed = 1337
-device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
-compile = False # use PyTorch 2.0 to compile the model to be faster
-exec(open('configurator.py').read()) # overrides from command line or config file
+device = "cuda"  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+dtype = (
+    "bfloat16"
+    if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
+    else "float16"
+)  # 'float32' or 'bfloat16' or 'float16'
+compile = False  # use PyTorch 2.0 to compile the model to be faster
+exec(open("configurator.py").read())  # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
-torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
-torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
-ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
+device_type = "cuda" if "cuda" in device else "cpu"  # for later use in torch.autocast
+ptdtype = {
+    "float32": torch.float32,
+    "bfloat16": torch.bfloat16,
+    "float16": torch.float16,
+}[dtype]
 
 model: GPT
 
 # model
-if init_from == 'resume':
+if init_from == "resume":
     # init from a model saved in a specific directory
-    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+    ckpt_path = os.path.join(out_dir, "ckpt.pt")
     checkpoint = torch.load(ckpt_path, map_location=device)
-    gptconf = GPTConfig(**checkpoint['model_args'])
+    gptconf = GPTConfig(**checkpoint["model_args"])
     model = GPT(gptconf)
-    state_dict = checkpoint['model']
-    unwanted_prefix = '_orig_mod.'
-    for k,v in list(state_dict.items()):
+    state_dict = checkpoint["model"]
+    unwanted_prefix = "_orig_mod."
+    for k, v in list(state_dict.items()):
         if k.startswith(unwanted_prefix):
-            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+            state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
     model.load_state_dict(state_dict)
-elif init_from.startswith('gpt2'):
+elif init_from.startswith("gpt2"):
     # init from a given GPT-2 model
     model = GPT.from_pretrained(init_from, dict(dropout=0.0))
 else:
-    raise ValueError(f"Unknown init_from value: {init_from}. Expected 'resume' or 'gpt2-<size>'.")
+    raise ValueError(
+        f"Unknown init_from value: {init_from}. Expected 'resume' or 'gpt2-<size>'."
+    )
 
 model.eval()
 model.to(device)
 if compile:
-    model = torch.compile(model) # requires PyTorch 2.0 (optional)
+    model = torch.compile(model)  # requires PyTorch 2.0 (optional)
 
 # look for the meta pickle in case it is available in the dataset folder
 modality: Literal["text", "audio"] = "text"
 load_meta = False
-if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']: # older checkpoints might not have these...
-    meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
+if (
+    init_from == "resume"
+    and "config" in checkpoint
+    and "dataset" in checkpoint["config"]
+):  # older checkpoints might not have these...
+    meta_path = os.path.join("data", checkpoint["config"]["dataset"], "meta.pkl")
     load_meta = os.path.exists(meta_path)
 if load_meta:
     print(f"Loading meta from {meta_path}...")
-    with open(meta_path, 'rb') as f:
+    with open(meta_path, "rb") as f:
         meta = pickle.load(f)
     # TODO want to make this more general to arbitrary encoder/decoder schemes
     modality = meta.get("modality", "text")
@@ -84,8 +105,9 @@ if load_meta:
         assert meta["encoding"] == "mu-law-256", (
             "Only mu-law encoding is supported for audio."
         )
+
         def encode(audio):
-            return (librosa.mu_compress(audio, mu=255) + 128)
+            return librosa.mu_compress(audio, mu=255) + 128
 
         def decode(tokens):
             # decode mu-law encoded audio
@@ -102,16 +124,16 @@ else:
     decode = lambda l: enc.decode(l)
 
 # encode the beginning of the prompt
-if start.startswith('FILE:'):
+if start.startswith("FILE:"):
     if modality == "text":
-        with open(start[len('FILE:'):], 'r', encoding='utf-8') as f:
+        with open(start[len("FILE:") :], "r", encoding="utf-8") as f:
             start = f.read()
     elif modality == "audio":
         # Read audio file using librosa
-        start, _ = librosa.load(start[len('FILE:'):], sr=meta["sample_rate"])
+        start, _ = librosa.load(start[len("FILE:") :], sr=meta["sample_rate"])
 else:
     if start == "\n":
-        start = np.array([0.])
+        start = np.array([0.0])
 
 start_ids = encode(start)
 x = torch.tensor(start_ids, dtype=torch.long, device=device)
@@ -122,9 +144,15 @@ file_prefix = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 # run generation
 with torch.no_grad():
-    ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+    ctx = (
+        nullcontext()
+        if device_type == "cpu"
+        else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+    )
     with ctx:
-        y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k, progress_bar=True)
+        y = model.generate(
+            x, max_new_tokens, temperature=temperature, top_k=top_k, progress_bar=True
+        )
         for k in range(num_samples):
             if modality == "text":
                 print(decode(y[k].tolist()))
@@ -133,4 +161,6 @@ with torch.no_grad():
                 audio = decode(y[k])
                 import soundfile as sf
 
-                sf.write(samples_dir / f"{file_prefix}_{k}.wav", audio, meta["sample_rate"])
+                sf.write(
+                    samples_dir / f"{file_prefix}_{k}.wav", audio, meta["sample_rate"]
+                )
